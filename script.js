23bea4dacfa3827c8d31e127734b0319260b2b51
Nayan <nayanbhusari02@gmail.com>
2026-03-06 11:59:45 +0530

    portfolio updated

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-document
-  .getElementById("contact-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
+const form = document.getElementById("contact-form");
 
-    emailjs.sendForm("service_m8zl1yb", "template_qeay8qd", this).then(
-      function () {
-        alert("✅ Message sent successfully!");
-        document.getElementById("contact-form").reset();
-      },
-      function (error) {
-        alert("❌ Failed to send message. Please try again later.");
-        console.error(error);
-      }
-    );
-  });
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  emailjs
+    .sendForm("service_m8zl1yb", "template_qeay8qd", this)
+    .then(function () {
+      alert("✅ Message sent successfully!");
+      form.reset();
+    })
+    .catch(function (error) {
+      console.error("EmailJS Error:", error);
+      alert("❌ Failed to send message.");
+    });
+});
